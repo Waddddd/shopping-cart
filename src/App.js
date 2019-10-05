a0bfac2f5335b2f11431a1b83afcdfe8f74eac57
Jiezhuo Lyu <wad@dhcp-10-105-151-201.wireless.northwestern.private>
2019-10-04 22:02:39 -0500

    styled static catalog page with images(modification)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Grid from '@material-ui/core/Grid';
 import GridList from '@material-ui/core/GridList';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -7,118 +8,20 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Checkbox from '@material-ui/core/Checkbox';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { makeStyles } from '@material-ui/core/styles';
 
-const App = () => {
-  const [data, setData] = useState({});
-  const products = Object.values(data);
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const response = await fetch('./data/products.json');
-      const json = await response.json();
-      setData(json);
-    };
-    fetchProducts();
-  }, []);
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(0.3),
+  },
+}));
 
-  
-    const [state, setState] = React.useState({
-      checked1: false,
-      checked2: false,
-      checked3: false,
-      checked4: false,
-      checked5: false,
-      checked6: false,
-      checked7: false,
-    });
-  
-  const handleChange = name => event => {
-      setState({ ...state, [name]: event.target.checked });
-    };
+const ProductList =({products}) => {
+  return(
 
-  return (
-      <div> 
-        <FormControlLabel
-        control={
-          <Checkbox
-            checked={state.checked1}
-            onChange={handleChange('checked1')}
-            value="checked1"
-            color="primary"
-          />
-        }
-        label="XS"
-      />
-       <FormControlLabel
-        control={
-          <Checkbox
-            checked={state.checked2}
-            onChange={handleChange('checked2')}
-            value="checked2"
-            color="primary"
-          />
-        }
-        label="S"
-      />
-       <FormControlLabel
-        control={
-          <Checkbox
-            checked={state.checked3}
-            onChange={handleChange('checked3')}
-            value="checked3"
-            color="primary"
-          />
-        }
-        label="M"
-      />
-       <FormControlLabel
-        control={
-          <Checkbox
-            checked={state.checked4}
-            onChange={handleChange('checked4')}
-            value="checked4"
-            color="primary"
-          />
-        }
-        label="ML"
-      />
-       <FormControlLabel
-        control={
-          <Checkbox
-            checked={state.checked5}
-            onChange={handleChange('checked5')}
-            value="checked5"
-            color="primary"
-          />
-        }
-        label="L"
-      />
-       <FormControlLabel
-        control={
-          <Checkbox
-            checked={state.checked6}
-            onChange={handleChange('checked6')}
-            value="checked6"
-            color="primary"
-          />
-        }
-        label="XL"
-      />
-       <FormControlLabel
-        control={
-          <Checkbox
-            checked={state.checked7}
-            onChange={handleChange('checked7')}
-            value="checked7"
-            color="primary"
-          />
-        }
-        label="XXL"
-      />
-      <GridList cellHeight={"auto"} cols={5} spacing={2}>
+  <GridList cellHeight={"auto"} cols={4}>
       {products.map(product => 
-      <Card key={product.sku} style={{maxWidth:400}}>
+      <Card key={product.sku} style={{maxWidth:250}}>
       <CardActionArea>
         <CardMedia
           component="img"
@@ -140,8 +43,58 @@ const App = () => {
       </CardActionArea>
     </Card>
     )}
-      </GridList>
-      </div>
+  </GridList>
+
+  );
+};
+
+const SizeBar = ({classes}) =>{
+  return(
+  <React.Fragment>
+  <Typography align="left" variant="h5" >
+  size:
+  </Typography>
+  <Button size="small" variant="outlined" color="primary" className={classes.button}>
+    S
+  </Button>
+  <Button size="small" variant="outlined" color="primary" className={classes.button}>
+    M
+  </Button>
+  <Button size="small" variant="outlined" color="primary" className={classes.button}>
+    L
+  </Button>
+  <Button size="small" variant="outlined" color="primary" className={classes.button}>
+    XL
+  </Button>
+  </React.Fragment>
+  );
+};
+
+const App = () => {
+  const [data, setData] = useState({});
+  const products = Object.values(data);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('./data/products.json');
+      const json = await response.json();
+      setData(json);
+    };
+    fetchProducts();
+  }, []);
+
+  const classes = useStyles();
+
+  return (
+  <React.Fragment>
+    <Grid container> 
+      <Grid item xs={2}>
+        <SizeBar classes={classes}/>
+      </Grid>
+      <Grid item xs={10}>
+        <ProductList products={products} />
+      </Grid>
+    </Grid>
+  </React.Fragment>
   );
 };
 
